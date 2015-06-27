50e2a0777d395f96d7c7dc4ec2c38f6e788e8cac
Jay Freeman (saurik) <saurik@saurik.com>
2015-06-26 21:18:40 -0700

    Apple's newer LLVM backends generate garbage code.

--- ios.sh
+++ ios.sh
@@ -2,6 +2,7 @@
 
 set -e -x
 
+sudo xcode-select --switch /Applications/Xcode-4.6.3.app
 cycc -i2.0 -oldid.arm -- -c -std=c++11 ldid.cpp -I.
 cycc -i2.0 -oldid.arm -- ldid.arm -x c sha1.c lookup2.c -I .
 
