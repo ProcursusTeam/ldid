043e30006254b050f5a5628bfaa84a0abf888e7a
Jay Freeman (saurik) <saurik@saurik.com>
2019-02-18 21:55:19 -0800

    Improve compatibility across different Cydia eras.

--- sysroot.sh
+++ sysroot.sh
@@ -21,3 +21,11 @@ merge libssl1.0_1.0.2q-1_iphoneos-arm.deb
 merge libssl-dev_1.0.2q-1_iphoneos-arm.deb
 merge libplist_2.0.0-1_iphoneos-arm.deb
 popd
+
+for lib in libplist libcrypto; do
+    for dylib in sysroot*/usr/lib/"${lib}".*.dylib; do
+        echo install_name_tool -id /usr/lib/"${lib}".dylib "${dylib}"
+        chmod 755 "${dylib}"
+        install_name_tool -id /usr/lib/"${lib}".dylib "${dylib}"
+    done
+done
