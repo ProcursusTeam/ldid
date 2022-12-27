d4be59e8c78f7424b8d952a5e810a4a99f2990a7
captinc <40917097+captinc@users.noreply.github.com>
2022-12-27 03:21:27 -0500

    Revert "Fix -U support for blank passwords"
    This reverts commit 30ce12250bff7b6ba84280db6471d91563b9acc8.

--- ldid.cpp
+++ ldid.cpp
@@ -115,7 +115,6 @@
     __attribute__((packed))
 
 std::string password;
-bool flag_U(false);
 std::vector<std::string> cleanup;
 bool flag_H(false);
 
@@ -1824,7 +1823,7 @@ class Stuff {
             exit(1);
         }
 
-        if (!flag_U) {
+        if (!PKCS12_verify_mac(value_, "", 0) && password.empty()) {
             char passbuf[2048];
             UI_UTIL_read_pw_string(passbuf, 2048, "Enter password: ", 0);
             password = passbuf;
@@ -3558,7 +3557,6 @@ int main(int argc, char *argv[]) {
             break;
 
             case 'U':
-                flag_U = true;
                 password = argv[argi] + 2;
             break;
 
